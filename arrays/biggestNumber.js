66d7d801982532c53289ca187afc85f52948337e
AswinT <26aswin@gmail.com>
2025-08-12 04:12:02 +0000

    starting

---
+++ arrays/biggestNumber.js
@@ -0,0 +1,7 @@
+const arr = [8,2,6,9,2,1,6]
+
+// have to find the biggest number
+let a = 0
+for(let num of arr){
+  console.log(num)
+}
---
+++ class/basic1.js
@@ -0,0 +1,8 @@
+class Student{
+  constructor(firstName,LastName,age){
+    this.firstName = firstName;
+    this.LastName = LastName;
+    this.age = age;
+  }
+}
+
